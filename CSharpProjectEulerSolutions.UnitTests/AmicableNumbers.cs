25cd33b15af2d0e5f92983aac2cabaf4d6b30e0a
Gerald Ncube <4821668+gncube@users.noreply.github.com>
2023-01-28 11:21:31 +0000

    setup first unit test - red test

---
+++ CSharpProjectEulerSolutions.UnitTests/AmicableNumbers.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharpProjectEulerSolutions.UnitTests
+{
+    internal class AmicableNumbers
+    {
+        public AmicableNumbers()
+        {
+        }
+
+        internal object ProperDivisors(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSharpProjectEulerSolutions.UnitTests/Problem_21Tests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+namespace CSharpProjectEulerSolutions.UnitTests;
+
+public class Problem_21Tests
+{
+    [Fact]
+    public void ReturnAnArrayOfProperDivisorsOfGivenNumber()
+    {
+        var number = 220;
+        var sut = new AmicableNumbers();
+        List<int> properDivisors = new List<int>() { 1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110 };
+        var actual = sut.ProperDivisors(number);
+
+        actual.Should().BeEquivalentTo(properDivisors);
+    }
+}
+
+/*
+* 1. Create function that gets an array of proper divisors of n
+* 2. Sum the array of proper divisors of n
+* 3. Find if the numbers are amicable
+* 
+*/

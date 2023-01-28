84f62b22ec78ef5dff5f7625dcfb4b267c248eaf
Gerald Ncube <4821668+gncube@users.noreply.github.com>
2023-01-28 11:26:46 +0000

    add red test for d(284)

--- CSharpProjectEulerSolutions.UnitTests/Problem_21Tests.cs
+++ CSharpProjectEulerSolutions.UnitTests/Problem_21Tests.cs
@@ -5,11 +5,22 @@ namespace CSharpProjectEulerSolutions.UnitTests;
 public class Problem_21Tests
 {
     [Fact]
-    public void ReturnAnArrayOfProperDivisorsOfGivenNumber()
+    public void ReturnAnArrayOfProperDivisorsOf220()
     {
         var number = 220;
         var sut = new AmicableNumbers();
         List<int> properDivisors = new List<int>() { 1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110 };
+        var actual = sut.ProperDivisors(number);
+
+        actual.Should().BeEquivalentTo(properDivisors);
+    }
+
+    [Fact]
+    public void ReturnAnArrayOfProperDivisorsOf284()
+    {
+        var number = 284;
+        var sut = new AmicableNumbers();
+        List<int> properDivisors = new List<int>() { 1, 2, 4, 71 , 142 };
         var actual = sut.ProperDivisors(number);
 
         actual.Should().BeEquivalentTo(properDivisors);
